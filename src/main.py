573c7358314d2c4e747052df461885c258c9d660
Donlon <idonlon@hotmail.com>
2022-08-19 22:55:23 +0800

    update main.py

--- src/main.py
+++ src/main.py
@@ -6,20 +6,29 @@ import math
 import os
 import random
 import time
-from email import utils as email_utils
+from email.utils import parsedate
 
 from retry import retry
 import requests
 import schedule
 import yaml
 
-data_dir = os.getenv('NRTOOL_DATA_PATH') or '/data'
-log_dir = os.getenv('NRTOOL_LOG_PATH') or os.path.join(data_dir, 'log')
+
+default_ua = 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Mobile Safari/532.12'
+
+data_dir = os.getenv('NRTOOL_DATA_PATH', '/data')
+log_dir = os.getenv('NRTOOL_LOG_PATH', os.path.join(data_dir, 'log'))
+request_ua = os.getenv('NRTOOL_USER_AGENT', default_ua)
+webapi_endpoint = os.getenv('NRTOOL_API_ENDPOINT')
+webapi_test_endpoint = os.getenv('NRTOOL_API_TEST_ENDPOINT', webapi_endpoint)
+
+if not webapi_endpoint:
+    raise Exception('NRTOOL_LOG_PATH is not set')
 
 
-def log(task_id, date, payload, response):
+def log_task(task_id, date, payload, response):
     os.makedirs(log_dir, exist_ok=True)
-    with open(os.path.join(log_dir, 'report_%s_%s.log' % (str(task_id), date)), 'w', encoding='utf-8') as f:
+    with open(os.path.join(log_dir, f'report_{task_id}_{date}.log'), 'w', encoding='utf-8') as f:
         json.dump({
             'task_id': task_id,
             'payload': payload,
@@ -28,10 +37,10 @@ def log(task_id, date, payload, response):
 
 
 @retry(exceptions=requests.exceptions.RequestException, tries=10, delay=10)
-def send_request_2(post_data, cookie):
-    r = requests.post('https://app.bupt.edu.cn/ncov/wap/default/save',
+def send_request_with_retry(post_data, cookie):
+    r = requests.post(webapi_endpoint,
                       headers={
-                          'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Mobile Safari/532.12',
+                          'User-Agent': request_ua,
                           'DNT': '1',
                           'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                           'Cookie': cookie,
@@ -56,15 +65,18 @@ def send_request(task_id, data, uid, cookie):
     }
     print(json.dumps(post_data))
 
-    r = send_request_2(post_data, cookie)
+    r = send_request_with_retry(post_data, cookie)
 
-    log(task_id, date, post_data, r.json())
+    log_task(task_id, date, post_data, r.json())
 
 
 def rayleigh_dist(sigma, upbound=math.inf):
     u = random.random()
-    x = sigma * math.sqrt(-2 * math.log(u))
-    if x > upbound:
+    if u > 0:
+        x = sigma * math.sqrt(-2 * math.log(u))
+    else:
+        x = math.inf
+    if x >= upbound:
         if upbound > 0:
             return rayleigh_dist(sigma, upbound)
         else:
@@ -73,7 +85,7 @@ def rayleigh_dist(sigma, upbound=math.inf):
         return x
 
 
-def do_task_v(payload, cancel_job=False):
+def do_task(payload, cancel_job=False):
     print('do task:', json.dumps(payload))
 
     with open(payload['profile_path'], 'r', encoding='utf-8') as f:
@@ -85,16 +97,16 @@ def do_task_v(payload, cancel_job=False):
         return schedule.CancelJob
 
 
-def pre_do_task(payload):
+def do_task_check(payload):
     sigma = payload['rayleigh_sigma']
     upbound = payload['rayleigh_upbound']
     # delay a random period and then send the request
     if sigma > 5 and upbound > 5: # at least 5 secs
         delay_sec = math.floor(rayleigh_dist(sigma, upbound))
-        print('scheduled to send request after %d secs' % delay_sec)
-        schedule.every(delay_sec).seconds.do(do_task_v, payload, cancel_job=True)
+        print(f'scheduled to send request after {delay_sec} secs')
+        schedule.every(delay_sec).seconds.do(do_task, payload, cancel_job=True)
     else:
-        do_task_v(payload)
+        do_task(payload)
 
 
 def parse_time_string(s):
@@ -137,13 +149,13 @@ def create_task(task):
 
     profile_path = os.path.join(data_dir, task['profile'])
     if not os.path.exists(profile_path):
-        print('Error: id=%s: profile \'%s\' is not found' % (str(payload['id']), profile_path))
+        print(f"Error: id={payload['id']}: profile '{profile_path}' is not found")
         return
     with open(profile_path, 'r', encoding='utf-8') as f:
         try:
             yaml.load(f, Loader=yaml.FullLoader)
         except yaml.parser.ParserError as e:
-            print('Invalid YAML file %s: %s' % (profile_path, e))
+            print(f'Invalid YAML file {profile_path}: {e}')
             return
 
     payload['profile_path'] = profile_path
@@ -153,16 +165,16 @@ def create_task(task):
         payload['rayleigh_sigma'] = rayleigh_sigma
         if rayleigh_sigma is None:
             # TODO: check if rayleigh_sigma is a number
-            print('Error: unknown rayleigh_sigma format: %s' % rayleigh_sigma)
-            return False
+            print(f'Error: unknown rayleigh_sigma format: {rayleigh_sigma}')
+            return
 
     if 'rayleigh_upbound' in task:
         rayleigh_upbound = parse_time_string(task['rayleigh_upbound'])
         payload['rayleigh_upbound'] = rayleigh_upbound
         if rayleigh_upbound is None:
             # TODO: check if rayleigh_upbound is a number
-            print('Error: unknown rayleigh_upbound format: %s' % rayleigh_upbound)
-            return False
+            print(f'Error: unknown rayleigh_upbound format: {rayleigh_upbound}')
+            return
 
     if 'time' not in task:
         base_send_time = '07:00'
@@ -170,17 +182,16 @@ def create_task(task):
         base_send_time = task['time']
     payload['time'] = base_send_time
 
-    print('Loaded task: id=%s, profile=%s, uid=%d, time=%s+~%ds' % (
-            str(payload['id']), profile_path, payload['uid'],
-            base_send_time, payload['rayleigh_sigma']))
-    schedule.every().day.at(base_send_time).do(pre_do_task, payload)
+    print(f"Loaded task: id={payload['id']}, profile={profile_path}, uid={payload['uid']}, "
+          f"time={base_send_time}+~{payload['rayleigh_sigma']}s")
+    schedule.every().day.at(base_send_time).do(do_task_check, payload)
     return True
 
 
 def load_tasks():
     path = os.path.join(data_dir, 'tasks.yaml')
     if not os.path.exists(path):
-        print('Task configuration is not exist: %s' % path)
+        print(f'Task configuration is not exist: {path}')
         return
 
     with open(path, 'r', encoding='utf-8') as f:
@@ -195,8 +206,8 @@ def load_tasks():
 
 
 def fetch_server_time():
-    r = requests.get('https://app.bupt.edu.cn/ncov/wap/default/index')
-    t = email_utils.parsedate(r.headers['Date'])
+    r = requests.get(webapi_test_endpoint)
+    t = parsedate(r.headers['Date'])
     return time.mktime(t)
 
 
